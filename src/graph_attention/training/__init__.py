7b9b1f2f6ae1d5c8982790c02af4179fa71c579c
Suamoxi <79272028+Suamoxi@users.noreply.github.com>
2026-09-03 14:54:42 +0200

    M6: export training correctness primitives

--- src/graph_attention/training/__init__.py
+++ src/graph_attention/training/__init__.py
@@ -1,1 +1,20 @@
 """Optimization and training orchestration."""
+
+from .losses import SampleLossAggregate, sample_reduced_mse
+from .scaling import ChannelStandardizer, TaskStandardizers, fit_train_standardizers
+from .step import (
+    OptimizerStepResult,
+    equal_sample_ddp_backward_scale,
+    train_equal_sample_optimizer_step,
+)
+
+__all__ = [
+    "ChannelStandardizer",
+    "OptimizerStepResult",
+    "SampleLossAggregate",
+    "TaskStandardizers",
+    "equal_sample_ddp_backward_scale",
+    "fit_train_standardizers",
+    "sample_reduced_mse",
+    "train_equal_sample_optimizer_step",
+]
