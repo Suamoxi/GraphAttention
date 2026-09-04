b0a5a0d4bc3d64e8f8dc49f0fc7260239f0a2bb5
Suamoxi <79272028+Suamoxi@users.noreply.github.com>
2026-09-04 11:47:24 +0200

    Export M8 sparse transformer models

--- src/graph_attention/models/__init__.py
+++ src/graph_attention/models/__init__.py
@@ -1,5 +1,6 @@
 """Learnable model transformations."""
 
 from .baseline import NodeLinearBaseline
+from .sparse_transformer import SparseGraphTransformer, SparseMultiheadAttention
 
-__all__ = ["NodeLinearBaseline"]
+__all__ = ["NodeLinearBaseline", "SparseGraphTransformer", "SparseMultiheadAttention"]
