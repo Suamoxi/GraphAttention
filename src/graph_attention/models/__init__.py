d8a70d984f78cad7fdc79c3069cfe1f53510e134
Suamoxi <79272028+Suamoxi@users.noreply.github.com>
2026-09-08 14:38:29 +0200

    Export alternating dilated transformer

--- src/graph_attention/models/__init__.py
+++ src/graph_attention/models/__init__.py
@@ -1,6 +1,7 @@
 """Learnable model transformations."""
 
 from .baseline import NodeLinearBaseline
+from .dilated_transformer import AlternatingDilatedGeometricSparseGraphTransformer
 from .geometric_transformer import (
     GeometricSparseGraphTransformer,
     GeometricSparseMultiheadAttention,
@@ -8,6 +9,7 @@ from .geometric_transformer import (
 from .sparse_transformer import SparseGraphTransformer, SparseMultiheadAttention
 
 __all__ = [
+    "AlternatingDilatedGeometricSparseGraphTransformer",
     "GeometricSparseGraphTransformer",
     "GeometricSparseMultiheadAttention",
     "NodeLinearBaseline",
