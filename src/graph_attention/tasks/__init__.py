c769893aa0399bcd28abf4162069a80c07b3f250
Suamoxi <79272028+Suamoxi@users.noreply.github.com>
2026-09-09 15:36:49 +0200

    Export diffusion task

--- src/graph_attention/tasks/__init__.py
+++ src/graph_attention/tasks/__init__.py
@@ -1,6 +1,12 @@
 """Scientific learning objectives and task semantics."""
 
+from .diffusion import DiffusionDenoisingTask
 from .flow_matching import FlowMatchingTask
 from .regression import NodeRegressionBatch, NodeRegressionTask
 
-__all__ = ["FlowMatchingTask", "NodeRegressionBatch", "NodeRegressionTask"]
+__all__ = [
+    "DiffusionDenoisingTask",
+    "FlowMatchingTask",
+    "NodeRegressionBatch",
+    "NodeRegressionTask",
+]
