b838747ee9b2a36f28bf55b80fd2dde679cd4a61
Suamoxi <79272028+Suamoxi@users.noreply.github.com>
2026-09-14 15:59:27 +0200

    Export Improved DDPM task

--- src/graph_attention/tasks/__init__.py
+++ src/graph_attention/tasks/__init__.py
@@ -2,11 +2,21 @@
 
 from .diffusion import DiffusionDenoisingTask
 from .flow_matching import FlowMatchingTask
+from .improved_diffusion import (
+    ImprovedDiffusionDenoisingTask,
+    ImprovedDiffusionLoss,
+    ImprovedDiffusionProblem,
+    LossSecondMomentTimestepSampler,
+)
 from .regression import NodeRegressionBatch, NodeRegressionTask
 
 __all__ = [
     "DiffusionDenoisingTask",
     "FlowMatchingTask",
+    "ImprovedDiffusionDenoisingTask",
+    "ImprovedDiffusionLoss",
+    "ImprovedDiffusionProblem",
+    "LossSecondMomentTimestepSampler",
     "NodeRegressionBatch",
     "NodeRegressionTask",
 ]
